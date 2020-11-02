75f55320c2783f9becba4562c42a76aaba4613ac
John Mars <john@actualsize.com>
2020-11-02 10:31:15 -0500

    Start to add tests

---
+++ tests/vanilla/index.js
@@ -0,0 +1,11 @@
+console.log(`Hello, World!`);
+console.log('Hello, World!');
+console.log("Hello, World!");
+
+var abc = 1;
+let def = 1;
+const ghi = 1;
+
+console.log(abc, def, ghi)
+
+const BAD_KEY = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=`;
